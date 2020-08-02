63b76eb98a9a7de0f336cd71c6b742bf73b40f0a
Norme <antonnormelius@MacBook-Pro.local>
2020-08-03 00:18:14 +0200

    Docs changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,14 @@
 #
 import os
 import sys
+import mock
 sys.path.insert(0, os.path.abspath('../..'))
 #import sphinx_rtd_theme
+
+MOCK_MODULES = ['pandas', 'numpy', 'pybind11']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 
 # -- Project information -----------------------------------------------------
 
