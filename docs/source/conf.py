054d83072218631a6489928066e85192e24388a1
Norme <antonnormelius@MacBook-Pro.local>
2020-08-03 14:02:16 +0200

    Docs changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,6 @@ author = 'Anton Normelius'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'numpydoc']
-html4_writer = True
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,13 +52,10 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 #html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = [
-    'css/custom.css',
-]
