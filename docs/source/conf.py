442cdd002bed95554eaf2ff76b466c1384d76ed8
Norme <antonnormelius@MacBook-Pro.local>
2020-08-03 10:25:44 +0200

    Docs changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ author = 'Anton Normelius'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
