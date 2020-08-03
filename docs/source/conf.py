abaff7ffd404afef1b42bc1de21c3092512be474
Norme <antonnormelius@MacBook-Pro.local>
2020-08-03 12:07:10 +0200

    Docs changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ author = 'Anton Normelius'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'numpydoc']
+html4_writer = True
 
 
 # Add any paths that contain templates here, relative to this directory.
